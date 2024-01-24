d2f0a4927ac5d3d6021ea9ccc13315563fb66eee
zack-of-all-trades <hikerzack@outlook.com>
2024-01-24 11:39:08 -0500

    02-repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+  let repeatStr = '';
+  
+  if(num < 0) return 'ERROR';
+
+  while (num > 0){
+    repeatStr += string;
+    num--;
+  }
+  return repeatStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
