ce55c7f5bbc4d1f570f831e188fc45506fd8e857
zack-of-all-trades <hikerzack@outlook.com>
2024-01-25 11:35:07 -0500

    04 removeFromArray complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...takeAway) {
 
-};
+  newArray = array.filter(value => !takeAway.includes(value));
+  
+  return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
